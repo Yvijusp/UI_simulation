1bac69a61e4975943eec2c3fb9544921bde8179b
Yvijusp <yvijus@hotmail.com>
2021-10-20 12:31:11 +0300

    Text input component complete | Step component complete

--- react_project/src/App.tsx
+++ react_project/src/App.tsx
@@ -1,16 +1,9 @@
-import { teamMembers } from './assets/images';
-import TeamCard from './components/Cards/TeamCard';
 import { GlobalStyle } from './GlobalStyle';
 
 function App() {
   return (
     <div>
       <GlobalStyle />
-      <TeamCard
-        image={teamMembers[0]}
-        name='Devon lane'
-        position='Technician'
-      />
     </div>
   );
 }
