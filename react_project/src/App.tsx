e2fb650316f487033bc9e0028e0bac439c158e1b
Yvijusp <yvijus@hotmail.com>
2021-10-20 15:36:27 +0300

    Completed header with pages and routes

--- react_project/src/App.tsx
+++ react_project/src/App.tsx
@@ -1,7 +1,30 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/Header/Header';
+import { GlobalStyle } from './GlobalStyles';
+import AboutUs from './pages/AboutUs';
+import Blog from './pages/Blog';
+import BlogPage from './pages/BlogPage';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import ServicePage from './pages/ServicePage';
+import Services from './pages/Services';
 
 function App() {
-  return <Router></Router>;
+  return (
+    <Router>
+      <GlobalStyle />
+      <Header />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/services' component={Services} />
+        <Route path='/services/:id' component={ServicePage} />
+        <Route path='/aboutus' component={AboutUs} />
+        <Route path='/contacts' component={Contact} />
+        <Route exact path='/blog' component={Blog} />
+        <Route path='/blog/:id' component={BlogPage} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
---
+++ react_project/src/components/Header/Header.styled.ts
@@ -0,0 +1,55 @@
+import styled from 'styled-components';
+import { Container } from '../../GlobalStyles';
+
+export const StyledHeader = styled.header`
+  background-color: var(--white-color);
+
+  ${Container} {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+`;
+
+export const Logo = styled.div`
+  width: 121.41px;
+`;
+
+export const Nav = styled.nav`
+  display: flex;
+
+  ul {
+    display: flex;
+    align-items: center;
+    list-style: none;
+
+    li {
+      padding: 32px 40px;
+      a {
+        color: var(--black-color);
+        text-decoration: none;
+        font-size: var(--paragraph-sm);
+        font-weight: 600;
+        line-height: 28px;
+      }
+    }
+  }
+`;
+
+export const Line = styled.div`
+  border: 1px solid #939191;
+  opacity: 0.36;
+  margin: 24px 32px;
+`;
+
+export const Assitance = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 24px;
+
+  p:last-child {
+    font-weight: 600;
+    font-size: var(--paragraph-med);
+    line-height: 175%;
+  }
+`;
