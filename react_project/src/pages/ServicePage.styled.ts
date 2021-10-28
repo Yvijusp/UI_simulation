d90b11e1db0ec759232fa8e85921c3896bd54105
Yvijusp <yvijus@hotmail.com>
2021-10-28 17:18:07 +0300

    Finished single service page

--- react_project/src/pages/ServicePage.styled.ts
+++ react_project/src/pages/ServicePage.styled.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import { StyledServiceCard } from '../components/Cards/Card.styled';
 import { Logo } from '../components/Feature/Feature.styled';
+import { Title01 } from '../GlobalStyle';
 import { wrapper } from './Services.styled';
 
 export const ServicePageWrapper = styled.section`
@@ -11,7 +12,10 @@ export const ServicePageWrapper = styled.section`
   }
 `;
 
-export const ServiceContainer = styled.section``;
+export const ServiceContainer = styled.section`
+  display: flex;
+  gap: 96px;
+`;
 
 export const ServiceSideBar = styled.div`
   max-width: 328px;
@@ -44,4 +48,52 @@ export const SideBarFeatures = styled.div`
   }
 `;
 
-export const ServiceContent = styled.div``;
+export const ServiceContent = styled.div`
+  img {
+    margin-bottom: 56px;
+  }
+
+  h4 {
+    margin-bottom: 24px;
+  }
+
+  p {
+    font-size: var(--paragraph-sm);
+    font-weight: 600;
+    line-height: 28px;
+    color: var(--black-color);
+    opacity: 0.6;
+  }
+
+  p:first-of-type {
+    margin-bottom: 68px;
+  }
+
+  P:nth-of-type(2) {
+    margin-bottom: 32px;
+  }
+
+  ${Title01} {
+    margin-bottom: 24px;
+  }
+`;
+
+export const ServiceContentEnd = styled.div`
+  display: flex;
+  align-items: center;
+
+  span {
+    display: block;
+    font-size: var(--paragraph-med);
+    font-weight: 600;
+    line-height: 31.5px;
+    color: var(--black-color);
+    opacity: 0.6;
+    margin-left: 32.46px;
+  }
+`;
+
+export const Line = styled.div`
+  border: 3px solid #1e1b1b;
+  height: 112px;
+`;
--- react_project/src/pages/ServicePage.tsx
+++ react_project/src/pages/ServicePage.tsx
@@ -4,13 +4,17 @@ import Template from '../components/templates/Template';
 import { Container, Title01 } from '../GlobalStyle';
 import { features, otherServices } from '../templateData';
 import {
+  Line,
   ServiceContainer,
   ServiceContent,
+  ServiceContentEnd,
   ServicePageWrapper,
   ServiceSideBar,
   SideBarFeatures,
   SideBarServices,
 } from './ServicePage.styled';
+import Vehicle from '../assets/images/services/bigvehicle.png';
+import CTA from '../components/CTA/CTA';
 
 const ServicePage = () => {
   return (
@@ -41,10 +45,70 @@ const ServicePage = () => {
                 ))}
               </SideBarFeatures>
             </ServiceSideBar>
-            <ServiceContent></ServiceContent>
+            <ServiceContent>
+              <div>
+                <div>
+                  <img src={Vehicle} alt='vehicle' />
+                </div>
+                <h4>
+                  The warning lights on your dashboard are the most obvious
+                  starting point.
+                </h4>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Mauris dignissim dolor et dui bibendum aliquam. Mauris a quam
+                  sit amet dui auctor dictum eget a elit. Pellentesque varius
+                  diam risus, ut condimentum lorem volutpat vel. Nam vel orci
+                  pharetra eros pulvinar cursus nec quis tellus. Quisque feugiat
+                  tortor lectus, pretium interdum justo tincidunt a. Donec at
+                  congue lectus. Nulla facilisi. Phasellus consectetur sapien
+                  accumsan lectus tincidunt placerat. Etiam ornare nibh vel dui
+                  egestas, eu posuere metus convallis. Ut non urna a odio
+                  condimentum dictum. Proin egestas erat a orci ultrices, vitae
+                  bibendum libero posuere. Quisque laoreet tincidunt justo.
+                  Vestibulum congue dictum libero finibus vehicula. Aliquam nisi
+                  velit, ultricies eget enim vel, venenatis mollis ante.
+                  Maecenas sodales tristique quam. Suspendisse fringilla massa
+                  vel dolor ornare rhoncus. Nullam ut orci mattis leo varius
+                  laoreet sed mollis dui. Aenean placerat nec enim ut finibus.
+                  Maecenas suscipit nibh eu neque egestas, non condimentum mi
+                  bibendum. Sed est eros, molestie consectetur auctor non,
+                  lobortis quis tortor. Nam cursus imperdiet massa volutpat
+                  hendrerit. Sed suscipit ligula iaculis lorem sagittis
+                  tincidunt. Etiam pellentesque metus vel enim iaculis aliquam.
+                  Mauris at nisi sed elit gravida malesuada.
+                </p>
+                <Title01>
+                  Delaying a diagnosis and repair could lead to even bigger
+                  problems down the road.
+                </Title01>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                  Mauris dignissim dolor et dui bibendum aliquam. Mauris a quam
+                  sit amet dui auctor dictum eget a elit. Pellentesque varius
+                  diam risus, ut condimentum lorem volutpat vel. Nam vel orci
+                  pharetra eros pulvinar cursus nec quis tellus. Quisque feugiat
+                  tortor lectus, pretium interdum justo tincidunt a. Donec at
+                  congue lectus. Nulla facilisi. Phasellus consectetur sapien
+                  accumsan lectus tincidunt placerat. Etiam ornare nibh vel dui
+                  egestas, eu posuere metus convallis.
+                </p>
+                <ServiceContentEnd>
+                  <Line></Line>
+                  <span>
+                    Ut non urna a odio condimentum dictum. Proin egestas erat a
+                    orci ultrices, vitae bibendum libero posuere. Quisque
+                    laoreet tincidunt justo. Vestibulum congue dictum libero
+                    finibus vehicula. Aliquam nisi velit, ultricies eget enim
+                    vel, venenatis mollis ante.
+                  </span>
+                </ServiceContentEnd>
+              </div>
+            </ServiceContent>
           </ServiceContainer>
         </Container>
       </ServicePageWrapper>
+      <CTA />
     </Template>
   );
 };
